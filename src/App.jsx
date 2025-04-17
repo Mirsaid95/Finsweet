553b281d3b8269d71b90958d45ac07bfa2479bce
Akbaraliyev_Mirsaid <157018820+Mirsaid95@users.noreply.github.com>
2025-04-17 23:43:03 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/main-layout";
 import { Home } from "./components/home";
-import  Service  from "./components/page/service";
+import Service from "./components/page/service";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="service" element={<Service />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/service" element={<Service />} />
         </Route>
       </Routes>
     </>
